7080be519be1ac817a36ab9d9795023d0d6609e7
Jaimie de Vries <49305844+jaimiedevries@users.noreply.github.com>
2022-04-26 19:14:18 +0200

    click interactie toegevoegd

--- js/app.js
+++ js/app.js
@@ -1,4 +1,7 @@
-window.addEventListener('keydown', speelAudio);
+// Array met daarin alle 'toetsen'
+const alleDrumkitToetsen = document.querySelector('.toetsen');
+
+const alleToetsen = document.querySelectorAll('.toets');
 
 function verwijderTransitie (e) {
     if(e.propertyName !== 'transform') return;
@@ -9,24 +12,34 @@ function speelAudio (e){
     // Array met daarin alle toetsen
     const alleToetsen = document.querySelectorAll('.toets');
 
-    // Wanneer er een audio element bestaat met als data-key de toets die je invoert sla die dan op.
-    const audio = document.querySelector(`audio[data-key="${e.key}"]`);
+    let toets;
+    let audio;
 
-    // Wanneer er een class toets bestaat met als data-key de toets die je invoert sla die dan op.
-    const toets = document.querySelector(`.toets[data-key="${e.key}"]`);
-
+    // Hier bekijk ik of er sprake is geweest van een click event, zoniet dan ga ik uit van een keyevent en op basis daarvan 
+    if(e.type === 'click' && e.target.matches('.toets')){
+        // toets bevat nu het element waar op is geklikt.
+        toets = e.target;
+        // Wanneer er een class bestaat met als data-key degene waar ik op heb geklikt, sla dat dan op.
+        audio = document.querySelector(`audio[data-key="${e.target.dataset.key}"]`);
+    }else {
+        // Wanneer er een audio element bestaat met als data-key de toets die je invoert sla die dan op.
+        const audio = document.querySelector(`audio[data-key="${e.key}"]`);
+        // Wanneer er een class toets bestaat met als data-key de toets die je invoert sla die dan op.
+        const toets = document.querySelector(`.toets[data-key="${e.key}"]`);
+    }
     // Wanneer er geen matchende audio is gevonden stop dan de functie.
     if(!audio) return;
 
     // Zorgt ervoor dat de audio direct opnieuw afspeelt vanwege runtime.
     audio.currentTime = 0;
-
+    
     audio.play();
-
     toets.classList.add('playing');
 
     /* // Loop over alle toetsen en kijk naar een transitioned event. Wanneer er een toets is waarvan de 
     transitie is geeindigd trigger dan de verwijderTransitie knop om de class playing te verwijderen*/
     alleToetsen.forEach(toets => toets.addEventListener('transitionend', verwijderTransitie));
-
 }  
+
+window.addEventListener('keydown', speelAudio);
+alleDrumkitToetsen.addEventListener('click', speelAudio);
