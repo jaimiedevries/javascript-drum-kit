10a90ce2c56735c6aa820693343c9b0c8c658eb8
Jaimie de Vries <49305844+jaimiedevries@users.noreply.github.com>
2022-04-29 09:35:59 +0200

    update

--- js/app.js
+++ js/app.js
@@ -1,45 +1,52 @@
-// Array met daarin alle 'toetsen'
+;// Array met daarin alle 'toetsen'
 const alleDrumkitToetsen = document.querySelector('.toetsen');
-
 const alleToetsen = document.querySelectorAll('.toets');
 
-function verwijderTransitie (e) {
-    if(e.propertyName !== 'transform') return;
-    this.classList.remove('playing');
-};
+window.addEventListener('keydown', onEvent);
+alleDrumkitToetsen.addEventListener('click', onEvent);
 
-function speelAudio (e){
-    // Array met daarin alle toetsen
-    const alleToetsen = document.querySelectorAll('.toets');
+/* Loop over alle toetsen en kijk naar een transitioned event. Wanneer er een toets is waarvan de 
+   transitie is geeindigd trigger dan de verwijderTransitie knop om de class playing te verwijderen */
+alleToetsen.forEach(toets => toets.addEventListener('transitionend', verwijderTransitie));
 
-    let toets;
-    let audio;
 
+function onEvent(event) {
     // Hier bekijk ik of er sprake is geweest van een click event, zoniet dan ga ik uit van een keyevent en op basis daarvan 
-    if(e.type === 'click' && e.target.matches('.toets')){
-        // toets bevat nu het element waar op is geklikt.
-        toets = e.target;
-        // Wanneer er een class bestaat met als data-key degene waar ik op heb geklikt, sla dat dan op. 
-        audio = document.querySelector(`audio[data-key="${e.target.dataset.key}"]`);
-    }else {
-        // Wanneer er een audio element bestaat met als data-key de toets die je invoert sla die dan op.
-         audio = document.querySelector(`audio[data-key="${e.key}"]`);
-        // Wanneer er een class toets bestaat met als data-key de toets die je invoert sla die dan op.
-         toets = document.querySelector(`.toets[data-key="${e.key}"]`);
+    if (event.type === 'click' && event.target.matches('.toets')) {
+        speelAudio(
+            event.target,
+            getAudioElement(event.target.dataset.key)
+        );
+
+    } else if (event.type === 'keydown') {
+        speelAudio(
+            getToetsElement(event.key),
+            getAudioElement(event.key)
+        );
     }
+}
+
+function speelAudio(toets, audio) {
     // Wanneer er geen matchende audio is gevonden stop dan de functie.
-    if(!audio) return;
+    if (!audio) return;
 
     // Zorgt ervoor dat de audio direct opnieuw afspeelt vanwege runtime.
     audio.currentTime = 0;
-    
+
     audio.play();
     toets.classList.add('playing');
+}
 
-    /* // Loop over alle toetsen en kijk naar een transitioned event. Wanneer er een toets is waarvan de 
-    transitie is geeindigd trigger dan de verwijderTransitie knop om de class playing te verwijderen*/
-    alleToetsen.forEach(toets => toets.addEventListener('transitionend', verwijderTransitie));
-}  
+function verwijderTransitie(e) {
+    if (e.propertyName === 'transform') {
+        e.target.classList.remove('playing');
+    }
+};
 
-window.addEventListener('keydown', speelAudio);
-alleDrumkitToetsen.addEventListener('click', speelAudio);
+function getToetsElement(key) {
+    return document.querySelector(`.toets[data-key="${key}"]`);
+}
+
+function getAudioElement(key) {
+    return document.querySelector(`audio[data-key="${key}"]`);
+}
